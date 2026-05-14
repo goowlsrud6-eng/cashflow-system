cd68eb44060a722c0d1860f86d37b669fd08d108
goowlsrud6-eng <goowlsrud6@gmail.com>
2026-05-14 11:48:46 +0900

    Update money.py

--- money.py
+++ money.py
@@ -13,9 +13,9 @@ import io
 DEFAULT_TARGET_CNY = 210.0       # 🎯 CNY 목표 환율 (예: 210원)
 DEFAULT_TARGET_USD = 1460.0      # 🎯 USD 목표 환율 (예: 1450원)
 
-DEFAULT_BASE_DATE = "2026-05-12" # 🏦 초기 잔고 기준 날짜
+DEFAULT_BASE_DATE = "2026-05-14" # 🏦 초기 잔고 기준 날짜
 DEFAULT_BASE_CNY = 566541.36     # 🏦 초기 CNY 잔고
-DEFAULT_BASE_USD = 81605.95     # 🏦 초기 USD 잔고
+DEFAULT_BASE_USD = 79733.84     # 🏦 초기 USD 잔고
 # ===========================================================
 
 # -----------------------------------------------------------
